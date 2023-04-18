84a36c748fedc8db0bab7f368a02ab3786bd2e0b
Kez Anwar <kezanwar@gmail.com>
2023-04-18 14:26:10 +0100

    more work on tests

--- services/tests/geolocation.spec.js
+++ services/tests/geolocation.spec.js
@@ -1,18 +1,20 @@
 /* eslint-disable no-undef */
 import { getLongLat } from '../geolocation.services'
 
-test('mismatched postcode and address return undefined', async () => {
-  await expect(getLongLat({ address_line_1: '2 Eversley Road', postcode: 'M44 3G' })).resolves.toBeFalsy()
-})
+// --- GET LONG LAT TESTS
 
-test('mismatched postcode and address return undefined', async () => {
-  await expect(getLongLat({ address_line_1: '2 Eversley Road', postcode: 'M44' })).resolves.toBeTruthy()
-})
-
-test('mismatched postcode and address return undefined', async () => {
+test('matching postcode and address return truthy', async () => {
   await expect(getLongLat({ address_line_1: '2 Eversley Road', postcode: 'M20 2FL' })).resolves.toBeTruthy()
 })
 
 test('mismatched postcode and address return undefined', async () => {
+  await expect(getLongLat({ address_line_1: '2 Eversley Road', postcode: 'M44 3G' })).resolves.toBeFalsy()
+})
+
+test('mismatched postcode and address return truthy', async () => {
+  await expect(getLongLat({ address_line_1: '2 Eversley Road', postcode: 'M44' })).resolves.toBeTruthy()
+})
+
+test('mismatched postcode and address return truthy', async () => {
   await expect(getLongLat({ address_line_1: '2 Eversley Road', postcode: '23123' })).resolves.toBeTruthy()
 })
