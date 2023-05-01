608a7e8f6770d864a465cd07f163888c3d3d7849
Kez Anwar <kezanwar@gmail.com>
2023-05-01 20:33:19 +0100

    moved items into services, started adding voucher model

---
+++ models/Voucher.js
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose'
+import CategorySchemaWithIndex from './schemas/CategorySchemaWithIndex.js'
+import GeoSchema from './schemas/GeoSchema.js'
+
+const VoucherSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    start_date: {
+      type: Date,
+    },
+    end_date: {
+      type: Date,
+    },
+    redeems: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+    downloads: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+    cuisines: [CategorySchemaWithIndex],
+    dietary_requirements: [CategorySchemaWithIndex],
+    locations: [
+      {
+        id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'location',
+        },
+        geometry: GeoSchema,
+      },
+    ],
+    restaurant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'restaurant',
+    },
+  },
+  { timestamps: true }
+)
+
+VoucherSchema.methods.updateVoucher = async function (data) {
+  if (!data) throw new Error('no data passed to setup method')
+  const dataArr = Object.entries(data)
+  dataArr.forEach(([key, value]) => {
+    this[key] = value
+  })
+  await this.save()
+}
+
+VoucherSchema.methods.toClient = function () {
+  let returnToClient = this.toJSON()
+  delete returnToClient._id
+  delete returnToClient.__v
+  delete returnToClient.createdAt
+  delete returnToClient.updatedAt
+  return returnToClient
+}
+
+// Ensure virtual fields are serialised.
+VoucherSchema.set('toJSON', {
+  virtuals: true,
+})
+
+const Voucher = mongoose.model('voucher', VoucherSchema)
+
+Voucher.createIndexes()
+
+export default Voucher
