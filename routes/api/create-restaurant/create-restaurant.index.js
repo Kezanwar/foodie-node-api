3d5df100df6240dfaf0031719f1092742bb27c19
Kez Anwar <kezanwar@gmail.com>
2023-06-06 22:54:44 +0100

    removed unneccesary import

--- routes/api/create-restaurant/create-restaurant.index.js
+++ routes/api/create-restaurant/create-restaurant.index.js
@@ -29,7 +29,6 @@ import { email_addresses } from '../../../constants/email.js'
 import { RESTAURANT_IMAGES } from '../../../constants/images.js'
 import { appEnv } from '../../../base/base.js'
 import { createImageName, createImgUUID } from '../../../services/images/images.services.js'
-import { createUrlFromLink } from '../../../services/util/util.services.js'
 
 //* route POST api/create-restaurant/company-info (STEP 1)
 //? @desc STEP 1 either create a new restaurant and set the company info, reg step, super admin and status, or update existing stores company info and leave rest unchanged
