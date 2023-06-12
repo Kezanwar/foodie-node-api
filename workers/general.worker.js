618dbc719d50fe6a4261c516e850f24279f54a82
Kez Anwar <kezanwar@gmail.com>
2023-06-12 17:49:13 +0100

    changed cron to expire vouchers once furthest back timezone comes into the same day as gmt

--- workers/general.worker.js
+++ workers/general.worker.js
@@ -1,6 +1,6 @@
 import { define } from 'nanolith'
 import { hasMultipleTimezones, findCountryPhoneCode } from '../services/location/location.services.js'
-import { getTimezonesToExpire } from '../services/date/date.services.js'
+import { getTimezonesToExpire, expireDate } from '../services/date/date.services.js'
 import { resizeImg } from '../services/images/images.services.js'
 
 const generalWorker = await define({
@@ -8,6 +8,7 @@ const generalWorker = await define({
   getTimezonesToExpire,
   resizeImg,
   findCountryPhoneCode,
+  expireDate,
   // Functions don't have to be directly defined within the
   // object, they can be defined elsewhere outside, or even
   // imported from a totally different module.
