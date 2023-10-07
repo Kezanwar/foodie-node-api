be0db62c70b8d63c1f7095b5729abc5fffd50305
Kez Anwar <kezanwar@gmail.com>
2023-10-07 16:39:46 +0100

    options - added alocohlic cuisine types for bars

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+//    ___                __ __
+//  .'  _.-----.-----.--|  |__.-----.
+//  |   _|  _  |  _  |  _  |  |  -__|
+//  |__| |_____|_____|_____|__|_____|
+
 import express, { json } from 'express'
 import dotenv from 'dotenv'
 import mixpanel from 'mixpanel'
