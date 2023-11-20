496e99c75b4bc1db40adc10b99b67b2bcfde729f
Kez Anwar <kezanwar@gmail.com>
2023-11-20 16:10:31 +0000

    feature/customer-routes - added favouriting and unfavouriting to deal

--- routes/api/cust/deals/deals.index.js
+++ routes/api/cust/deals/deals.index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 const router = Router()
 import dotenv from 'dotenv'
-import { SendError, getID, throwErr } from '../../../utilities/utilities.js'
+import { SendError, throwErr } from '../../../utilities/utilities.js'
 import Deal from '../../../../models/Deal.js'
 import auth from '../../../../middleware/auth.middleware.js'
 import { generalWorkerService } from '../../../../services/workers/general.service.worker.js'
+import User from '../../../../models/User.js'
 
 dotenv.config()
 
@@ -224,18 +225,55 @@ router.post('/favourite', auth, async (req, res) => {
   } = req
   try {
     if (!deal_id || !location_id) throwErr('Deal/Location ID not passed', 401)
-    const deal = await Deal.findById(deal_id)
 
-    if (!deal) throwErr('Deal not found', 401)
+    const newDealFavourite = { user: user._id, location_id }
 
-    const location = deal.locations.find((l) => getID(l.location_id) === location_id)
+    const updateDeal = await Deal.updateOne(
+      {
+        _id: deal_id,
+        $and: [
+          { 'favourites.user': { $ne: newDealFavourite.user } },
+          { 'favourites.location_id': { $ne: newDealFavourite.location_id } },
+        ],
+      },
+      { $addToSet: { favourites: { user: user._id, location_id } } }
+    )
 
-    if (!location) throwErr('Location not found', 401)
+    if (!updateDeal.modifiedCount) {
+      throwErr('Deal not found or you have already favourited this deal', 401)
+    }
 
-    deal.favourites.push({ user: user._id, location_id })
-    user.favourites.push({ deal: deal_id, location_id })
+    const newUserFavourite = { deal: deal_id, location_id }
 
-    await Promise.all([deal.save(), user.save()])
+    await User.updateOne(
+      {
+        _id: req.user._id,
+        $and: [
+          { 'favourites.deal': { $ne: newUserFavourite.deal } },
+          { 'favourites.location_id': { $ne: newUserFavourite.location_id } },
+        ],
+      },
+      { $addToSet: { favourites: { deal: deal_id, location_id } } }
+    )
+
+    return res.json('Success')
+  } catch (error) {
+    SendError(res, error)
+  }
+})
+
+router.post('/unfavourite', auth, async (req, res) => {
+  const {
+    body: { location_id, deal_id },
+    user,
+  } = req
+  try {
+    if (!deal_id || !location_id) throwErr('Deal/Location ID not passed', 401)
+
+    const dealProm = Deal.updateOne({ _id: deal_id }, { $pull: { favourites: { user: user._id, location_id } } })
+    const userProm = User.updateOne({ _id: req.user._id }, { $pull: { favourites: { deal: deal_id, location_id } } })
+
+    await Promise.all([dealProm, userProm])
 
     return res.json('Success')
   } catch (error) {
