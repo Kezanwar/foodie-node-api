91b99ae591e4b1b64da4919548c4a86dcd50f439
Kez Anwar <kezanwar@gmail.com>
2024-01-21 21:43:23 +0000

    feature/customer-routes - more refinements to single deal query - added distance calc to pipeline

---
+++ utilities/distance-pipeline.js
@@ -0,0 +1,100 @@
+const METER_TO_MILE_CONVERSION = 0.00062137
+const r = 6371 // km
+const p = Math.PI / 180
+
+export const calculateDistancePipeline = (LAT, LONG, locationField, newDistanceField) => {
+  return [
+    {
+      $addFields: {
+        dLon: {
+          //dLon = (lon2-lon1) * Math.PI / 180
+          $multiply: [
+            {
+              $subtract: [
+                {
+                  $arrayElemAt: [`${locationField}.geometry.coordinates`, 0],
+                },
+                LONG,
+              ],
+            },
+            p,
+          ],
+        },
+        dLat: {
+          //dLat = (lat2-lat1) * Math.PI / 180
+          $multiply: [
+            {
+              $subtract: [
+                {
+                  $arrayElemAt: [`${locationField}.geometry.coordinates`, 1],
+                },
+                LAT,
+              ],
+            },
+            p,
+          ],
+        },
+      },
+    },
+    {
+      $addFields: {
+        a1: {
+          // Math.sin(dLat/2) * Math.sin(dLat/2)
+          $multiply: [{ $sin: { $divide: ['$dLat', 2] } }, { $sin: { $divide: ['$dLat', 2] } }],
+        },
+        a2: {
+          //Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2)
+          $multiply: [
+            {
+              $cos: {
+                $multiply: [{ $multiply: [LAT, p] }],
+              },
+            },
+            {
+              $cos: {
+                $multiply: [
+                  {
+                    $multiply: [
+                      {
+                        $arrayElemAt: [`${locationField}.geometry.coordinates`, 1],
+                      },
+                      p,
+                    ],
+                  },
+                ],
+              },
+            },
+            { $sin: { $divide: ['$dLon', 2] } },
+            { $sin: { $divide: ['$dLon', 2] } },
+          ],
+        },
+      },
+    },
+    //a3 = a1 + a2
+    {
+      $addFields: {
+        a3: { $add: ['$a1', '$a2'] },
+      },
+    },
+    //c = 2 * Math.atan2(Math.sqrt(a3), Math.sqrt(1-a3));
+    {
+      $addFields: {
+        c: {
+          $multiply: [
+            2,
+            {
+              $atan2: [{ $sqrt: '$a3' }, { $sqrt: { $subtract: [1, '$a3'] } }],
+            },
+          ],
+        },
+      },
+    },
+    {
+      $addFields: {
+        [newDistanceField]: {
+          $multiply: [{ $multiply: [{ $multiply: [r, '$c'] }, 1000] }, METER_TO_MILE_CONVERSION],
+        },
+      },
+    },
+  ]
+}
--- validation/customer/deal.js
+++ validation/customer/deal.js
@@ -2,18 +2,42 @@ import { object, string } from 'yup'
 import mongoose from 'mongoose'
 
 //field is either params or body
-export const singleDealSchema = (field) =>
-  object({
-    [field]: object({
-      location_id: string()
-        .required('Location ID is required')
-        .test('Location ID must be a valid ID', function (val) {
-          return mongoose.isValidObjectId(val)
-        }),
-      deal_id: string()
-        .required('Deal ID is required')
-        .test('Deal ID must be a valid ID', function (val) {
-          return mongoose.isValidObjectId(val)
-        }),
-    }),
-  })
+export const singleDealSchema = object({
+  query: object({
+    location_id: string()
+      .required('Location ID is required')
+      .test('Location ID must be a valid ID', function (val) {
+        return mongoose.isValidObjectId(val)
+      }),
+    deal_id: string()
+      .required('Deal ID is required')
+      .test('Deal ID must be a valid ID', function (val) {
+        return mongoose.isValidObjectId(val)
+      }),
+    lat: string()
+      .required('Lat is required')
+      .test('Lat must be able to cast to a number', function (val) {
+        return !isNaN(val)
+      }),
+    long: string()
+      .required('Long is required')
+      .test('Long must be able to cast to a number', function (val) {
+        return !isNaN(val)
+      }),
+  }),
+})
+
+export const favouriteDealSchema = object({
+  body: object({
+    location_id: string()
+      .required('Location ID is required')
+      .test('Location ID must be a valid ID', function (val) {
+        return mongoose.isValidObjectId(val)
+      }),
+    deal_id: string()
+      .required('Deal ID is required')
+      .test('Deal ID must be a valid ID', function (val) {
+        return mongoose.isValidObjectId(val)
+      }),
+  }),
+})
