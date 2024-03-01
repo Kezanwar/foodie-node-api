e6135695e5376f6b31346f8f372b9d030b447ac1
Kez Anwar <kezanwar@gmail.com>
2024-03-01 19:31:37 +0000

    feature/discover - added blogs and caching to discover route

--- utilities/regex.js
+++ utilities/regex.js
@@ -1,3 +1,5 @@
+import { decode } from 'html-entities'
+
 export const matchAllCases = (str) => new RegExp(`^${str}$`, 'i')
 
 export function createUrlFromString(str) {
@@ -8,4 +10,14 @@ const dateStringRegEx = new RegExp('((?:19|20)\\d\\d)-(0?[1-9]|1[012])-([12][0-9
 
 export function matchesDateString(str) {
   return dateStringRegEx.test(str)
+}
+
+export function removeTags(str) {
+  if (str === null || str === '') return false
+  else str = str.toString()
+
+  // Regular expression to identify HTML tags in
+  // the input string. Replacing the identified
+  // HTML tag with a null string.
+  return decode(str.replace(/(<([^>]+)>)/gi, ''))
 }
