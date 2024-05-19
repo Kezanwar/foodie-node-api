a83ea523ca710039bd41631046966a0b94142cdc
Kez Anwar <kezanwar@gmail.com>
2024-05-19 20:25:17 +0100

    feature/push-notifications - optimized new deal notification

--- services/worker/tasks/notifications.js
+++ services/worker/tasks/notifications.js
@@ -12,16 +12,21 @@ export const createNewDealNotificationMessages = (locations, deal) => {
   // locations = [{ restaurant_name, location_name, followers }]
   const d = JSON.parse(deal)
   const l = JSON.parse(locations).filter((has) => !!has.followers.length)
+  const title = 'Foodie'
+
   const messages = []
   for (const location of l) {
+    const body = `${location.restaurant_name} ${location.location_name} has posted a new deal: ${d.name}`
+    const data = { type: NOTICATION_NATIVE_APP_TYPES.SINGLE_DEAL, location_id: location._id, deal_id: d._id }
+
     for (const follower of location.followers) {
       for (const token of follower.push_tokens) {
         messages.push({
           to: token,
           sound: 'default',
-          title: `Foodie`,
-          body: `${location.restaurant_name} ${location.location_name} has posted a new deal: ${d.name} - Check it out!`,
-          data: { type: NOTICATION_NATIVE_APP_TYPES.SINGLE_DEAL, location_id: location._id, deal_id: d._id },
+          title,
+          body,
+          data,
         })
       }
     }
