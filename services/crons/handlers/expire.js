6061846355642507d6e1a48d01b431c043d78262
Kez Anwar <kezanwar@gmail.com>
2025-02-03 22:35:38 +0000

    feature/no-end-date-deals - bumped expiry cron check by a minute to always be ahead of a zerod hour datetime

--- services/crons/handlers/expire.js
+++ services/crons/handlers/expire.js
@@ -1,10 +1,11 @@
 import Err from '#app/services/error/index.js'
 import Mixpanel from '#app/services/mixpanel/index.js'
 import DB from '#app/services/db/index.js'
+import { addMinutes } from 'date-fns'
 
 const expireDealsHandler = async () => {
   try {
-    const expiredResults = await DB.RBulkExpireDealsFromDate(new Date())
+    const expiredResults = await DB.RBulkExpireDealsFromDate(addMinutes(new Date(), 1))
     Mixpanel.trackExpiredDeals(expiredResults)
   } catch (error) {
     Err.log(error)
